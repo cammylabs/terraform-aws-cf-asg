7d054b45da143919bb26040302c3b265e099fb6d
David Vuong <david.vuong256@gmail.com>
2017-11-20 13:37:02 +1100

    fix(variables): value must be an int

--- variables.tf
+++ variables.tf
@@ -43,7 +43,7 @@ variable "max-update-batch-size" {
 }
 
 variable "timeout-in-minutes" {
-  default = "30 minutes"
+  default = 30
   description = "The amount of time that can pass before the CF stack status becomes CREATE_FAILED"
 }
 variable "pause-time" {
